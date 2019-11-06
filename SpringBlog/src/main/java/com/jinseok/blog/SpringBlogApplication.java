71d569ed548655858635681c81e8d8f6d17b5c79
psych <psych@DESKTOP-0RFMQ60>
2019-11-06 15:42:13 +0900

    커밋

--- SpringBlog/src/main/java/com/jinseok/blog/SpringBlogApplication.java
+++ SpringBlog/src/main/java/com/jinseok/blog/SpringBlogApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//
-@MapperScan(basePackages ="com.jinseok.blog.mvc")
 public class SpringBlogApplication {
 
 	//Spring Boot MainMethod
---
+++ SpringBlog/src/main/java/com/jinseok/blog/mvc/config/AplicationConfig.java
@@ -0,0 +1,34 @@
+package com.jinseok.blog.mvc.config;
+
+
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+
+@Configuration
+public class AplicationConfig {
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource ds) throws Exception {
+	    SqlSessionFactoryBean factory = new SqlSessionFactoryBean();
+	    factory.setDataSource(ds);
+	    factory.setMapperLocations(
+	        new PathMatchingResourcePatternResolver().getResources("classpath:/mappers/*_SQL.xml") // A
+	    );
+	    return factory.getObject();
+	}
+
+	@Bean
+	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory factory) {
+	    return new SqlSessionTemplate(factory);
+
+	}
+
+}
